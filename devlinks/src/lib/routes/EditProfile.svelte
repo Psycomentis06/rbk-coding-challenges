b4fae150840d88c75713cdab351c855db2653b89
psycomentis06 <alibenamor30@gmail.com>
2023-09-16 00:54:01 +0100

    Initial boilerplate

---
+++ devlinks/src/lib/routes/EditProfile.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import {showDefaultHeader, showEditHeader} from '../stores/header'
+
+  onMount(() => {
+    showDefaultHeader.set(false)
+    showEditHeader.set(true)
+  })
+</script>
+
+Profile
