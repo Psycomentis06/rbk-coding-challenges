05f6915699807c32c7370f7e1dfd5795948d45af
psycomentis06 <alibenamor30@gmail.com>
2023-09-16 00:52:25 +0100

    Routing

--- devlinks/src/App.svelte
+++ devlinks/src/App.svelte
@@ -1,1 +1,16 @@
-<h1 class="btn btn-primary">Svelte App</h1>
+<script lang="ts">
+  import {Router, Route} from 'svelte-routing'
+  import Home from './lib/routes/Home.svelte'
+  import EditProfile from './lib/routes/EditProfile.svelte'
+  import EditLinks from './lib/routes/EditLinks.svelte'
+  import Header from './lib/components/Header.svelte';
+
+</script>
+  <Router>
+  <Header />
+  <div>
+    <Route path="/" component={Home} />
+    <Route path="/edit" component={EditProfile} />
+    <Route path="/edit/profile" component={EditLinks} />
+  </div>
+  </Router>
