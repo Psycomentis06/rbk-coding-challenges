5d9f34a5234cf4b33869673dc31cbf3a195c2b63
psycomentis06 <alibenamor30@gmail.com>
2023-09-16 16:49:29 +0100

    Save user form

--- devlinks/src/lib/routes/EditProfile.svelte
+++ devlinks/src/lib/routes/EditProfile.svelte
@@ -1,11 +1,40 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import {showDefaultHeader, showEditHeader} from '../stores/header'
+  import UploadProfilePicture from '../components/UploadProfilePicture.svelte';
+  import ProfileForm from '../components/ProfileForm.svelte';
+  import { getUser, saveUser } from '../services/user-profile';
+  import {userProfileStore} from '../stores/profile'
+    import { addToast } from '../services/toast';
+  let firstname
+  let lastname
+  let email
+  let avatar
 
-  onMount(() => {
-    showDefaultHeader.set(false)
-    showEditHeader.set(true)
-  })
+  const saveUserInfo = () => {
+    let user = getUser()
+    if (user) {
+      user.name = firstname
+      user.lastName = lastname
+      user.email = email
+      user.avatar = avatar
+    } else {
+      user = {
+        name: firstname,
+        lastName: lastname,
+        email: email,
+        avatar: avatar,
+        links: []
+      }
+    }
+    saveUser(user)
+    userProfileStore.set(user)
+    addToast({
+      message: "Your changes have been successfully save!",
+      closeBtn: true
+    })
+  }
 </script>
-
-Profile
+<h1 class="text-3xl font-bold">Profile Details</h1>
+<h5 class="mt-5 text-neutral-500">Add your details to create a personal touch to your profile.</h5>
+<UploadProfilePicture bind:avatar={avatar} />
+<ProfileForm bind:firstname={firstname} bind:lastname={lastname} bind:email={email} />
+<button on:click={saveUserInfo} class="btn btn-primary my-4 btn-wide normal-case block ml-auto mr-0">Save</button>
